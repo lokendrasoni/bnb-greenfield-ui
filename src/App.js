ef5ba0950096b2af53b59ce85ea6ae0b78d4f626
Lokendra Soni <lokendrasoni10@gmail.com>
2024-09-04 11:33:52 +0530

    INIT

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    ></div>
+  );
+}
+
+export default App;
